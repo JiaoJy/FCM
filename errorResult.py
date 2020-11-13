b2af4231d1c63fd9b59076d9792ea766120613ad
JiaoJy <jiaojiy@qq.com>
2020-11-13 16:13:03 +0800

    release

--- errorResult.py
+++ errorResult.py
@@ -72,3 +72,33 @@ def result(data_pre,data_test,path):
     temp = np.array(temp)
     temp = pd.DataFrame(temp)
     temp.to_csv(path,header=None)
+    
+def result_alt(data_pre,data_test,path):
+    path = 'error_Result/result_prediction_'+path+'.csv'
+    Mwucha = MSE(data_pre,data_test)
+    Rwucha = RMSE(Mwucha)
+    Bm = np.linalg.norm(data_pre-data_test)/data_pre.shape[0]
+    jd = jdwucha(data_pre,data_test)
+    xd = xdwucha(data_pre,data_test)
+    print('绝对误差：{}'.format(jd))
+    print('相对误差：{}'.format(xd))
+    print('欧式距离：{}'.format(Bm))
+    print('MSE：{}'.format(Mwucha))
+    print('RMSE:{}'.format(Rwucha))
+    temp = [0]*6
+    temp[0] = ["C1","C2","C3","C4","C5"]
+    temp[1] = jd
+    temp[2] = xd
+    temp[3] = Mwucha
+    temp[4] = Rwucha
+    temp[5] = [Bm,0,0,0,0]
+    temp = np.array(temp)
+    temp = pd.DataFrame(temp)
+    temp.to_csv(path,header=None)
+
+def drawAll_alt(data_pre,data_test):
+    drawPre("C1",data_pre[:,0],data_test[:,0],dataNum = data_test.shape[0])
+    drawPre("C2",data_pre[:,1],data_test[:,1],dataNum = data_test.shape[0])
+    drawPre("C3",data_pre[:,2],data_test[:,2],dataNum = data_test.shape[0])
+    drawPre("C4",data_pre[:,3],data_test[:,3],dataNum = data_test.shape[0])
+    drawPre("C5",data_pre[:,4],data_test[:,4],dataNum = data_test.shape[0])
